8bf7be6641c89e0e21c28397c4fdbf4730bbd156
Shahin <mansouri.math.kntu@gmail.com>
2024-12-25 12:38:53 +0330

    add context: day: int & next_day: bool

--- tetris_bot/home/views.py
+++ tetris_bot/home/views.py
@@ -10,44 +10,46 @@ def home(request):
         context = dict()
         coins, create = Coin.objects.get_or_create(user=request.user)
         context['coins'] = coins
+        context['day'] = coins.day
+        context['next_day'] = coins.is_valid()
         return render(request, 'home/home.html', context=context)
-    
-    # دریافت داده‌ها از URL (query parameters)
-    username = request.GET.get("username")
-    telegram_id = request.GET.get("telegram_id")
-    first_name = request.GET.get("first_name")
-    last_name = request.GET.get("last_name")
+    return HttpResponse("خطا: تمام فیلدها باید پر شوند.", status=400)
+    # # دریافت داده‌ها از URL (query parameters)
+    # username = request.GET.get("username")
+    # telegram_id = request.GET.get("telegram_id")
+    # first_name = request.GET.get("first_name")
+    # last_name = request.GET.get("last_name")
 
-    # بررسی اینکه آیا تمام اطلاعات موجود است یا خیر
-    if not all([username, telegram_id, first_name, last_name]):
-        return HttpResponse("خطا: تمام فیلدها باید پر شوند.", status=400)
+    # # بررسی اینکه آیا تمام اطلاعات موجود است یا خیر
+    # if not all([username, telegram_id, first_name, last_name]):
+    #     return HttpResponse("خطا: تمام فیلدها باید پر شوند.", status=400)
 
-    # بررسی اینکه آیا کاربر با این `telegram_id` قبلاً وجود دارد یا نه
-    existing_user = TelegramUser.objects.filter(telegram_id=telegram_id).first()
+    # # بررسی اینکه آیا کاربر با این `telegram_id` قبلاً وجود دارد یا نه
+    # existing_user = TelegramUser.objects.filter(telegram_id=telegram_id).first()
 
-    if existing_user:
-        # وارد کردن کاربر به سیستم (login)
-        login(request, existing_user)
-        return render(request, 'home/home.html')
+    # if existing_user:
+    #     # وارد کردن کاربر به سیستم (login)
+    #     login(request, existing_user)
+    #     return render(request, 'home/home.html')
 
-    # ذخیره داده‌ها در دیتابیس و لاگین کاربر
-    user = TelegramUser(
-        username=username,
-        telegram_id=telegram_id,
-        first_name=first_name,
-        last_name=last_name
-    )
-    user.set_password("defaultpassword")  # اضافه کردن پسورد به صورت هش شده
-    user.save()
+    # # ذخیره داده‌ها در دیتابیس و لاگین کاربر
+    # user = TelegramUser(
+    #     username=username,
+    #     telegram_id=telegram_id,
+    #     first_name=first_name,
+    #     last_name=last_name
+    # )
+    # user.set_password("defaultpassword")  # اضافه کردن پسورد به صورت هش شده
+    # user.save()
 
-    # ایجاد رکورد Coin برای کاربر جدید
-    user_coin = Coin(user=user, coin_amount=10**3)  # اختصاص مقدار اولیه 10 برای سکه‌ها
-    user_coin.save()
+    # # ایجاد رکورد Coin برای کاربر جدید
+    # user_coin = Coin(user=user, coin_amount=10**3)  # اختصاص مقدار اولیه 10 برای سکه‌ها
+    # user_coin.save()
 
-    # وارد کردن کاربر به سیستم (login)
-    login(request, user)
+    # # وارد کردن کاربر به سیستم (login)
+    # login(request, user)
 
-    # انتقال به صفحه دیگری پس از ذخیره موفقیت‌آمیز
+    # # انتقال به صفحه دیگری پس از ذخیره موفقیت‌آمیز
     return render(request, 'home/home.html')
 
 
@@ -66,3 +68,4 @@ def coin_day(request, day):
         return JsonResponse({'success': False, 'message': f'Try the next day.'}, status=400)
     return JsonResponse({'success': False, 'message': f'Application must be by post only.'}, status=405)
         
+
